2b7c19c941cbedc22791d27076b6f6132fe01185
Pelumiii1 <adesokanpelumi14@gmail.com>
2024-11-13 08:49:30 +0100

    bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,9 @@ import { useEffect } from "react";
 export default function Home() {
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const storedToken = localStorage.getItem("token");
 
   const { token } = useAppSelector((state) => state.admin);
-  if (storedToken) {
+  if (token) {
     return router.push("/analytics");
   } else {
     return router.push("/login");
